535686c81e780bb255ccc44e728a56e9d399ee66
Spixt586 <ojtipanluisa@gmail.com>
2026-04-15 21:51:57 -0500

    feat: create function calcularDisponible

--- simulador.js
+++ simulador.js
@@ -1,3 +1,20 @@
-function calcularDisponible(){
+function calcular(){
+    let ingresosFloat = 0;
+    let egresosFloat = 0;
+    let cmpIngresos;
+    let cmpEgresos;
+    let cmpValorDisponibleFloat;
+    cmpIngresos = document.getElementById("txtIngresos");
+    cmpEgresos = document.getElementById("txtEgresos");
+    let valorDisponibleFloat;
+    ingresosFloat = parseFloat(cmpIngresos.value);
+    egresosFloat= parseFloat(cmpEgresos.value);
+    valorDisponibleFloat = calcularDisponible(ingresosFloat, egresosFloat);
+    console.log(valorDisponibleFloat);
+    cmpValorDisponibleFloat = document.getElementById("spnDisponible");
+    cmpValorDisponibleFloat.innerText = valorDisponibleFloat
+}
+
+function calcularCapacidadPago(montoDisponible){
     
 }
